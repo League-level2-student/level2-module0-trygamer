84c46db1611370c124b0b665c7deaa91445e8636
trygamer <League@AdministorsiMac.attlocal.net>
2018-08-03 18:29:21 -0700

    rr.ip

--- src/arrays/_00_ArrayCheatSheet.java
+++ src/arrays/_00_ArrayCheatSheet.java
@@ -1,18 +1,40 @@
 package arrays;
 
+import java.util.Random;
+
 public class _00_ArrayCheatSheet {
 	public static void main(String[] args) {
 		//1. make an array of 5 Strings
-		
+		String[] ms = new String[5];
+		ms[0]="billy";
+		ms[1]="scone";
+		ms[2]="hi";
+		ms[3]="pow";
+		ms[4]="hola";
+		int[] ii = new int[50];
+		Random r = new Random();
 		//2. print the third element in the array
+		System.out.println(ms[2]);
 		
 		//3. set the third element to a different value
-		
+		ms[2]="joking";
 		//4. print the third element again
-		
+		System.out.println(ms[2]);
 		//5. use a for loop to print all the values in the array
 		//   BE SURE TO USE THE ARRAY'S length VARIABLE
-		
+		int smallest =0;
+		for(int i= 0; i<50; i++) {
+			
+			int y=r.nextInt(50);
+			ii[i]=y;
+			System.out.println(ii[i]);
+			
+			if(smallest<y) {
+				 smallest=y;
+			}
+			
+		}
+		System.out.println(smallest);
 		
 		//6. make an array of 50 integers
 		
--- src/arrays/_01_RobotRace.java
+++ src/arrays/_01_RobotRace.java
@@ -1,16 +1,22 @@
 package arrays;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class _01_RobotRace {
 	//1. make a main method
-
+public static void main(String[] args) {
 	//2. create an array of 5 robots.
-
+	Robot[] r = new Robot[5];
 	//3. use a for loop to initialize the robots.
-
+	for (int i = 0; i < r.length; i++) {
 		//4. make each robot start at the bottom of the screen, side by side, facing up
-
+		r[i]= new Robot();
+		r[i].miniaturize();
+		r[i].setY(500);
+		r[i].setX(100+i*100);
 	//5. use another for loop to iterate through the array and make each robot move 
 	//   a random amount less than 50.
+
 
 	//6. use a while loop to repeat step 5 until a robot has reached the top of the screen.
 
@@ -19,4 +25,5 @@ public class _01_RobotRace {
 	//8. try different races with different amounts of robots.
 
 	//9. make the robots race around a circular track.
+}	}
 }
